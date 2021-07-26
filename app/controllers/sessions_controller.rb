155213c7b0281b0bb6569bca582a46efcc550282
Senayson <senaialema@gmail.com>
2021-07-26 06:58:44 +0000

    Create UI links Register Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     if user&& user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to :new
+      redirect_to '/sessions/new'
     end
 
   end
