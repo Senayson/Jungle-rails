47a5f6d2c0526480adad4a7363befae4329b93e7
Senayson <senaialema@gmail.com>
2021-07-27 03:33:24 +0000

    Create admin categories table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -21,7 +21,7 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:product).permit(
+    params.require(:category).permit(
       :name,
       :description
     )
