8c056d942bb30d84cfe3e7a8901e441908516a2a
Senayson <senaialema@gmail.com>
2021-07-27 22:17:06 +0000

    Create examples

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+ #Validate input data
+  describe 'Validations:' do
+    before(:each) do
+      @category = Category.new(name: 'Sports')
+    @product = @category.products.new({name: 'Velodrome Bicycle', price: 20, quantity: 4})
+    end
+
+   
+      it 'Saves product when all fields are filled' do
+      expect(@product.save).to eq true
+      end
+
+      it 'Fails to save when a name field is nil' do
+        @product.name = nil
+        expect(@product.save).to eq false
+        expect(@product.errors.messages[:name]).to eq ["can't be blank"]
+      end
+
+      it 'Fails to save when price field is nil' do
+        @product.price = nil
+        puts @product.price 
+        expect(@product.save).to eq false
+        # expect(@product.errors.messages[:price]).to eq ["is not a number", "can't be blank"]
+      end
+
+      it 'Fails to save when price field is not a number' do
+        @product.price = "20"
+        puts @product.price 
+        expect(@product.save).to eq false
+        # expect(@product.errors.messages[:price]).to eq ["is not a number", "can't be blank"]
+      end
+
+      it 'Fails to save when quantity field is nil' do
+        @product.quantity = nil
+        expect(@product.save).to eq false
+        expect(@product.errors.messages[:quantity]).to eq ["can't be blank"]
+      end
+
+      it 'Fails to save when category field is nil' do
+        puts @product.category.name
+        @product.category = nil
+        expect(@product.save).to eq false
+        expect(@product.errors.messages[:category]).to eq ["can't be blank"]
+      end
+end
+
+end
