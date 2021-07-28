99ecd4beee8951698c773e8b73fed062e14fe0ef
Senayson <senaialema@gmail.com>
2021-07-28 23:52:36 +0000

    Assert page renders to product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+    scenario "Able to navigate to product details page" do
+      visit root_path
+      el = page.find('article:first-child header').find('a')
+      
+    el.click
+
+      expect(page).to have_css ('.product-detail')
+      # puts page.html
+
+    end
+
+
+
+
+end
